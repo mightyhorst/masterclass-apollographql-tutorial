2fd812af3de08c2484318677b3bb0d19844ee969
mitni455 <nickymitch@gmail.com>
2020-10-21 10:39:17 +1100

    :rocket: code: add bookTrips mutation resolver

--- workspaces/server/src/resolvers.js
+++ workspaces/server/src/resolvers.js
@@ -10,7 +10,24 @@ module.exports = {
         login: async (_, { email }, { dataSources }) => {
             const user = await dataSources.userAPI.findOrCreateUser({ email });
             if (user) return Buffer.from(email).toString('base64');
-        }
+        },
+        bookTrips: async (_, { launchIds }, { dataSources }) => {
+            const results = await dataSources.userAPI.bookTrips({ launchIds });
+            const launches = await dataSources.launchAPI.getLaunchesByIds({
+                launchIds,
+            });
+
+            return {
+                success: results && results.length === launchIds.length,
+                message:
+                    results.length === launchIds.length
+                        ? 'trips booked successfully'
+                        : `the following launches couldn't be booked: ${launchIds.filter(
+                            id => !results.includes(id),
+                        )}`,
+                launches,
+            };
+        },
     },
     Mission: {
         // The default size is 'LARGE' if not provided
