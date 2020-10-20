49dbbd1e78b3c59aa8fc925770aa510482985758
mitni455 <nickymitch@gmail.com>
2020-10-21 10:12:50 +1100

    :rocket: code: Add User resolver

--- workspaces/server/src/resolvers.js
+++ workspaces/server/src/resolvers.js
@@ -18,4 +18,19 @@ module.exports = {
         isBooked: async (launch, _, { dataSources }) =>
             dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
     },
+    User: {
+        trips: async (_, __, { dataSources }) => {
+            // get ids of launches by user
+            const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
+
+            if (!launchIds.length) return [];
+
+            // look up those launches by their ids
+            return (
+                dataSources.launchAPI.getLaunchesByIds({
+                    launchIds,
+                }) || []
+            );
+        },
+    },
 };
