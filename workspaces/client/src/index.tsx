34c02880371ef6108b7039fa1e8ad3b9f2f10787
mitni455 <nickymitch@gmail.com>
2020-10-21 17:41:43 +1100

    :rocket: code: add vanilla client query

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -9,3 +9,18 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
     uri: 'http://localhost:4000/',
     cache: new InMemoryCache()
 });
+
+client
+    .query({
+        query: gql`
+          query TestQuery {
+            launch(id: 56) {
+              id
+              mission {
+                name
+              }
+            }
+          }
+        `
+    })
+    .then(result => console.log(result));
