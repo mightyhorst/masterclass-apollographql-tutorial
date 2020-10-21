4af614069ee60e0c4a97a7f3d03fdd61ae0c73a8
mitni455 <nickymitch@gmail.com>
2020-10-21 19:46:15 +1100

    :rocket: code: workspaces/client/src/pages/launches.tsx
    Here, we're defining a query to fetch a list of launches by calling the `launches` query from our
    schema. The `launches` query returns an object type with a list of launches, in addition to the
    `cursor` of the paginated list and whether or not the list `hasMore` launches. We need to wrap
    the query with the `gql` function in order to parse it into an AST.

--- workspaces/client/src/pages/launches.tsx
+++ workspaces/client/src/pages/launches.tsx
@@ -45,7 +45,29 @@ const GET_LAUNCHES = gql`
 interface LaunchesProps extends RouteComponentProps {}
 
 const Launches: React.FC<LaunchesProps> = () => {
-  return <div />;
+  const {
+    data,
+    loading,
+    error
+  } = useQuery<
+    GetLaunchListTypes.GetLaunchList,
+    GetLaunchListTypes.GetLaunchListVariables
+  >(GET_LAUNCHES);
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR</p>;
+  if (!data) return <p>Not found</p>;
+
+  return (
+    <Fragment>
+      <Header />
+      {data.launches &&
+        data.launches.launches &&
+        data.launches.launches.map((launch: any) => (
+          <LaunchTile key={launch.id} launch={launch} />
+        ))}
+    </Fragment>
+  );
 }
 
 export default Launches;
