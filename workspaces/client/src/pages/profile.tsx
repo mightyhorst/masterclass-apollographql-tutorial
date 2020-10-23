246c0b974cbe17155ed8e62fe2b3ea8f245c18a4
mitni455 <nickymitch@gmail.com>
2020-10-23 12:01:15 +1100

    :rocket: code: Add Profile page

--- workspaces/client/src/pages/profile.tsx
+++ workspaces/client/src/pages/profile.tsx
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react'; // preserve-line
+import { gql, useQuery } from '@apollo/client'; // preserve-line
+
+import { Loading, Header, LaunchTile } from '../components'; // preserve-line
+import { LAUNCH_TILE_DATA } from './launches'; // preserve-line
 import { RouteComponentProps } from '@reach/router';
+import * as GetMyTripsTypes from './__generated__/GetMyTrips';
+
+export const GET_MY_TRIPS = gql`
+  query GetMyTrips {
+    me {
+      id
+      email
+      trips {
+        ...LaunchTile
+      }
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
 
 interface ProfileProps extends RouteComponentProps {}
 
