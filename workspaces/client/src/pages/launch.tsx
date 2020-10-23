07d58d794b55df9860eedde200ee8bb1acc82eed
mitni455 <nickymitch@gmail.com>
2020-10-23 11:47:08 +1100

    :rocket: code: add Get Launch Details to Launch page

--- workspaces/client/src/pages/launch.tsx
+++ workspaces/client/src/pages/launch.tsx
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react'; // preserve-line
+import { gql, useQuery } from '@apollo/client'; // preserve-line
+
+import { Loading, Header, LaunchDetail } from '../components'; // preserve-line
+import { ActionButton } from '../containers'; // preserve-line
 import { RouteComponentProps } from '@reach/router';
+import * as LaunchDetailsTypes from './__generated__/LaunchDetails';
+
+export const GET_LAUNCH_DETAILS = gql`
+  query LaunchDetails($launchId: ID!) {
+    launch(id: $launchId) {
+      id
+      site
+      isBooked
+      rocket {
+        id
+        name
+        type
+      }
+      mission {
+        name
+        missionPatch
+      }
+    }
+  }
+`;
 
 interface LaunchProps extends RouteComponentProps {
 
