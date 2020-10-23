3e9e565a4cb20c87351b78c2795151f2262afc79
mitni455 <nickymitch@gmail.com>
2020-10-23 11:47:52 +1100

    :rocket: code: add Launch props and Component to Launch page

--- workspaces/client/src/pages/launch.tsx
+++ workspaces/client/src/pages/launch.tsx
@@ -26,11 +26,34 @@ export const GET_LAUNCH_DETAILS = gql`
 `;
 
 interface LaunchProps extends RouteComponentProps {
-
+  launchId?: any;
 }
 
-const Launch: React.FC<LaunchProps> = () => {
-  return <div />;
+const Launch: React.FC<LaunchProps> = ({ launchId }) => {
+  const {
+    data,
+    loading,
+    error
+  } = useQuery<
+    LaunchDetailsTypes.LaunchDetails,
+    LaunchDetailsTypes.LaunchDetailsVariables
+  >(GET_LAUNCH_DETAILS,
+    { variables: { launchId } }
+  );
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR: {error.message}</p>;
+  if (!data) return <p>Not found</p>;
+
+  return (
+    <Fragment>
+      <Header image={data.launch && data.launch.mission && data.launch.mission.missionPatch}>
+        {data && data.launch && data.launch.mission && data.launch.mission.name}
+      </Header>
+      <LaunchDetail {...data.launch} />
+      <ActionButton {...data.launch} />
+    </Fragment>
+  );
 }
 
 export default Launch;
