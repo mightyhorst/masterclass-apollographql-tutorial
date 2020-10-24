a9b9b2065c876f36d23e18fc2d89a14a12630198
mitni455 <nickymitch@gmail.com>
2020-10-24 12:47:00 +1100

    :rocket: code: add profile page props and component

--- workspaces/client/src/pages/profile.tsx
+++ workspaces/client/src/pages/profile.tsx
@@ -22,7 +22,26 @@ export const GET_MY_TRIPS = gql`
 interface ProfileProps extends RouteComponentProps {}
 
 const Profile: React.FC<ProfileProps> = () => {
-  return <div />;
+  const { data, loading, error } = useQuery<GetMyTripsTypes.GetMyTrips, any>(
+    GET_MY_TRIPS,
+    { fetchPolicy: "network-only" } // highlight-line
+  );
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR: {error.message}</p>;
+  if (data === undefined) return <p>ERROR</p>;
+
+  return (
+    <Fragment>
+      <Header>My Trips</Header>
+      {data.me && data.me.trips.length ? (
+        data.me.trips.map((launch: any) => (
+          <LaunchTile key={launch.id} launch={launch} />
+        ))
+      ) : (
+        <p>You haven't booked any trips</p>
+      )}
+    </Fragment>
+  );
 }
 
 export default Profile;
