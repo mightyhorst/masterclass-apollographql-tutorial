b3765df57a43366e9b54bd60c5ebbed1ae492690
mitni455 <nickymitch@gmail.com>
2020-10-24 16:38:17 +1100

    :rocket: code: add Login Page query

--- workspaces/client/src/pages/login.tsx
+++ workspaces/client/src/pages/login.tsx
@@ -1,4 +1,17 @@
-import React from 'react';
+import React from 'react'; 
+import { gql, useMutation } from '@apollo/client'; 
+
+import { LoginForm, Loading } from '../components'; 
+import * as LoginTypes from './__generated__/login';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!) {
+    login(email: $email) {
+      id
+      token
+    }
+  }
+`;
 
 export default function Login() {
   return <div />;
